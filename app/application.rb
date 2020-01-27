701613e886a3d04573fd6a5057fe044248c3b30c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-27 01:25:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +15,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+      
     else
       resp.write "Path Not Found"
     end
